f4e09d811ce35a05b56169e6ead14ba30e7cc8ec
Shane Johnson <shane.john316@gmail.com>
2024-09-01 14:26:16 -0500

    Added lined to ask the user which types of characters they want to include in their password.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // Assignment code here
 const generatePassword = function () {
-  let passwordChar = [];
+  let passwordChar = '';
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseLetters = lowercaseLetters.toUpperCase();
+  const numbers = '0123456789';
+  const symbols = ` !"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`
 
   const passwordLength = parseInt(prompt("Please choose a password length between 8 and 128 characters"));
 
   if (passwordLength === NaN || passwordLength < 8 || passwordLength > 128) {
     alert("Please choos a password length between 8 and 128 characters");
+    return;
   }
+
+  const includeLowercase = confirm("Include lowercase letters?");
+  const includeUppercase = confirm("Include uppercase letters?");
+  const includeNums = confirm("Include numbers?");
+  const includeSymbols = confirm("Include symbols?");
+
 }
 
 // Get references to the #generate element
